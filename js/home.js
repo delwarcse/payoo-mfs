729b79681be815b7eb9d7dc444283d15a68abbf6
Mohammad Delwar Hosen <94333408+delwarcse@users.noreply.github.com>
2025-02-11 10:33:43 +0600

    first commit

---
+++ js/home.js
@@ -0,0 +1,35 @@
+//add money to the account
+
+//step 1 - add an event handler to the add money button inside the form
+document.getElementById('btn-add-money')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        //step 2 - get money to be added to the account   
+        const inputMoney = document.getElementById('input-add-money').value;
+        console.log(inputMoney);
+        const inputPin = document.getElementById('input-pin').value;
+        console.log(inputPin);
+
+        //step 3 - pin Varify(temporal)
+        if (inputPin === "1234") {
+            console.log('money add');
+            //step 4 - get the current balance
+            const currentBalance = document.getElementById('ac-balance').innerText;
+            console.log(currentBalance);
+
+        //stef 5 - add  inputMoney with balance
+        const inputMoneyNumber= parseFloat(inputMoney);
+        const balanceMoneyNumber= parseFloat(currentBalance);
+        const newBalance=inputMoneyNumber+balanceMoneyNumber;
+        console.log(newBalance);
+
+        //step 6 - update the balance in the UI/DOM
+        document.getElementById('ac-balance').innerText=newBalance;
+        } 
+        else {
+            alert('Failed to add money');
+        }
+
+
+    });
---
+++ js/login.js
@@ -0,0 +1,25 @@
+//step 1 - set event  handler
+document.getElementById('button-login')
+    .addEventListener('click', function (event) {
+        //step 2 - prevent default behaviour (prevent reloading Browser)
+        event.preventDefault();
+        // console.log('Login Button Clicked');
+
+        //step 3 - get the phone number & pin number
+        const phoneNumber = document.getElementById('phone-number').value;
+        const pinNumber = document.getElementById('pin-number').value;
+        // console.log(phoneNumber, pinNumber);
+
+        //step 4 - validte phone & pin
+        //the temporary way for matching
+        if (phoneNumber === '4' && pinNumber === '123') {
+            console.log('you are Logged In');
+            //step 5 - allow user to use the website
+            window.location.href= '/home.html';
+
+        }
+        else {
+            alert('wrong input!')
+        }
+
+    });
